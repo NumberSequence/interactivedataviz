c3e616728fd69fe1fc91816540812f45aa324ef2
Allan <aambris@gradcenter.cuny.edu>
2022-02-20 17:55:43 -0500

    Button counter w/ timestamp

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,13 @@
 console.log('hello world');
+
+const label1 = document.getElementById("count-label1")
+const label2 = document.getElementById("count-label2")
+const button = document.getElementById("count-submit")
+
+let CountNum = 0;
+
+function ClickCountUpdate() {
+CountNum++
+label1.innerText = "Number of clicks since load/refresh: " + CountNum
+label2.innerText = "Last click: " + new Date()
+    }
